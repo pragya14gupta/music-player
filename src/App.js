141c1eab88354cbadea1294b498746fb88d314ed
Pragya Gupta <83855575+pragya14gupta@users.noreply.github.com>
2022-11-16 23:57:09 +0530

    dash

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import Login from './login';
+import Dashboard from './Dashboard';
+
+const code = new URLSearchParams(window.location.search).get('code')
 
 function App() {
   return (
     <>
-    <Login/></>
+    return code ? <Dashboard code = {code}/> : <Login/>
+    </>
   );
 }
 
--- src/login.js
+++ src/login.js
@@ -4,7 +4,7 @@ import { Container } from "react-bootstrap"
 const AUTH_URL =
   "https://accounts.spotify.com/authorize?client_id=2835a1b69bab474f9433748d590a1127&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
 
-export default function Login() {
+const Login=() =>{
   return (
     <Container
       className="d-flex justify-content-center align-items-center"
@@ -16,3 +16,6 @@ export default function Login() {
     </Container>
   )
 }
+
+
+export default Login;
